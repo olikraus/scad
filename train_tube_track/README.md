1338fef9cfb77eedc1a764cdd21d810a95f56cd6
kraus <olikraus@gmail.com>
2019-12-22 19:34:16 +0100

    update

--- train_tube_track/README.md
+++ train_tube_track/README.md
@@ -1,2 +1,5 @@
+[https://licensebuttons.net/l/by-nc-sa/3.0/88x31.png](https://licensebuttons.net/l/by-nc-sa/3.0/88x31.png) License: https://creativecommons.org/licenses/by-nc-sa/4.0
+
+
 ![gif/ttt_animation.gif](gif/ttt_animation.gif)
 
--- train_tube_track/do_animation.bash
+++ train_tube_track/do_animation.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in {71..71}
+for i in {0..71}
   do 
     printf -v ii "%04d" $i
     let h=($i-30)*2
     echo $i $h
-    openscad -Dy=$i scad/ttt_animation.scad --render --camera -100,20,$h,0,0,0 --imgsize=640,480 -o $ii.png
+    openscad -Dy=$i scad/ttt_animation.scad --render --camera -100,20,$h,0,0,-10 --imgsize=320,240 -o $ii.png
  done
  
  convert -delay 15 0*.png -loop 0 gif/ttt_animation.gif
