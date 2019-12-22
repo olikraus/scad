75ee4fd98e75c5d259b54a808a07768ab92460e3
kraus <olikraus@gmail.com>
2019-12-22 19:16:10 +0100

    png and stl

--- train_tube_track/README.md
+++ train_tube_track/README.md
@@ -1,2 +1,2 @@
-![ttt_animation.gif](ttt_animation.gif)
+![gif/ttt_animation.gif](gif/ttt_animation.gif)
 
--- train_tube_track/do_animation.bash
+++ train_tube_track/do_animation.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for i in {0..71}
+for i in {71..71}
   do 
     printf -v ii "%04d" $i
     let h=($i-30)*2
     echo $i $h
-    openscad -Dy=$i ttt_animation.scad --render --camera -100,20,$h,0,0,0 --imgsize=640,480 -o $ii.png
+    openscad -Dy=$i scad/ttt_animation.scad --render --camera -100,20,$h,0,0,0 --imgsize=640,480 -o $ii.png
  done
  
- convert -delay 15 0*.png -loop 0 ttt_animation.gif
+ convert -delay 15 0*.png -loop 0 gif/ttt_animation.gif
  
